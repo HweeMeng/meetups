3c8118ea8c17193ff7b6843fe0e34abc00247873
lekhweemeng <lek.hweemeng@live.com>
2020-10-02 12:51:59 +0800

    added in api for attendees and available dates

--- meetupbackend/meetup/serializers.py
+++ meetupbackend/meetup/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import Meetup
+from .models import Attendees
+from .models import Available
 
 class MeetupSerializer(serializers.ModelSerializer):
   class Meta:
     model = Meetup
     fields = ('id', 'meetup', 'description')
+
+class AttendeesSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Attendees
+    fields = ('id', 'name', 'plus_one', 'plus_half', 'meetup')
+
+class AvailableSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Available
+    fields = ('id', 'date', 'meal', 'person')
--- meetupbackend/meetup/views.py
+++ meetupbackend/meetup/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets          # add this
+from .serializers import MeetupSerializer      # add this
+from .serializers import AttendeesSerializer      # add this
+from .serializers import AvailableSerializer      # add this
+from .models import Meetup                     # add this
+from .models import Attendees
+from .models import Available
 
-# Create your views here.
+class MeetupView(viewsets.ModelViewSet):       # add this
+  serializer_class = MeetupSerializer          # add this
+  queryset = Meetup.objects.all()              # add this
+  
+class AttendeesView(viewsets.ModelViewSet):       # add this
+  serializer_class = AttendeesSerializer          # add this
+  queryset = Attendees.objects.all()              # add this
+
+class AvailableView(viewsets.ModelViewSet):       # add this
+  serializer_class = AvailableSerializer          # add this
+  queryset = Available.objects.all()              # add this
