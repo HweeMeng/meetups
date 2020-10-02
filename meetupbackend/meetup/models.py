9f645de089d535c20d88b8474001d84ef3925a18
lekhweemeng <lek.hweemeng@live.com>
2020-10-02 12:03:03 +0800

    added in backend

---
+++ meetupbackend/meetup/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Meetup(models.Model):
+  meetup = models.CharField(max_length=120)
+  description = models.TextField()
+
+  def __str__(self):
+    return self.meetup
+
+class Attendees(models.Model):
+    name = models.CharField(max_length=100)
+    plus_one = models.BooleanField(default=False)
+    plus_half = models.BooleanField(default=False)
+    meetup = models.ForeignKey(Meetup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Available(models.Model):
+    date = pub_date = models.DateField()
+    meal = models.CharField(max_length=100)
+    person = models.ForeignKey(Attendees, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.date
