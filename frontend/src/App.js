beafc577e076c8d3651a5c098377f7a874cfb6c6
lekhweemeng <lek.hweemeng@live.com>
2020-10-07 12:10:26 +0800

    updated and able to show all dates person

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,9 @@ class App extends React.Component {
           meetup:{
             meetup:"",
             description:"",
-            attendees:[],
+            attendees:[{ 
+              dates:[]
+            }],
             id:""
           }
         }
@@ -44,14 +46,44 @@ class App extends React.Component {
         console.log("inside x functions", name)
       })
     }
+    
+    attendeesDates = () =>{
+      console.log("this is attendeesDates", this.state.meetup.attendees[0]);
+      console.log("this is where the ttendees.length is", this.state.meetup.attendees.length)
+      this.state.meetup.attendees.map((attendees)=>{
+        console.log("map function runs", attendees.dates)
+        return (<div>
+                  <h2>{attendees.name}</h2>
+                  <p>{attendees.dates}
+                  </p>
+                </div>)
+        })
+      // for (var i = 0; i<this.state.meetup.attendees.length; i++){
+      //   console.log("for loop running")
+      //   console.log("this is the attendees details: ", this.state.meetup.attendees[i])
+      //   for (var j = 0; j<this.state.meetup.attendees[i].dates.length; j++){
+      //     console.log("2nd for loop running")
+      //     console.log("this is the dates details: ", this.state.meetup.attendees[i].dates[j])
+      //     return <p>{this.state.meetup.attendees[i].dates}</p>
+      //   }
+      // }
+      
+    }
+
   render(){
     console.log("this is where the this.state is", this.state)
       return (<div className="App">
                 <h2>{this.state.meetup.meetup}</h2>
                 <h3>{this.state.meetup.description}</h3>
                 {this.state.meetup.attendees.map((name) => {
-                  return <button key="name.id"> {name.name} </button>;
+                  return <div>
+                          <button className={name.name} key="name.id"> {name.name} </button>
+                          <p>{name.dates.map((date)=>{
+                            return <p>{date}</p>
+                          })}</p>
+                        </div>;
                 })}
+                {this.attendeesDates()}
               </div>
           );
   }
