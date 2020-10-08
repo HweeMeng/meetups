00391f47c27648ede95be4a7f52b3cf94a1c4e6d
lekhweemeng <lek.hweemeng@live.com>
2020-10-08 17:35:04 +0800

    to aws

--- meetupbackend/meetup/serializers.py
+++ meetupbackend/meetup/serializers.py
@@ -3,19 +3,19 @@ from .models import Meetup
 from .models import Attendees
 from .models import Available
 
-class MeetupSerializer(serializers.ModelSerializer):
-    attendees = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Meetup
-        fields = ('id', 'meetup', 'description','attendees')
-
 class AttendeesSerializer(serializers.ModelSerializer):
     dates = serializers.StringRelatedField(many=True)
     
     class Meta:
         model = Attendees
         fields = ('id', 'name', 'plus_one', 'plus_half', 'meetup', 'dates')
+
+class MeetupSerializer(serializers.ModelSerializer):
+    attendees = AttendeesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Meetup
+        fields = ('id', 'meetup', 'description','attendees')
 
 class AvailableSerializer(serializers.ModelSerializer):
   class Meta:
